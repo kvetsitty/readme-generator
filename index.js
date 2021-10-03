b2c8226a72194b6dc86dad80134dfd4bc3558ea4
n1zhon1 <kristineetsitty@gmail.com>
2021-10-03 12:19:20 -0700

    added questions to array

--- index.js
+++ index.js
@@ -6,8 +6,39 @@ const fs = require("fs")
 const questions = [
     {
         name: 'project',
-        type: "input",
-        message: "What is the name of your project?" 
+        type: 'input',
+        message: 'What is the name of your project?' 
+    },
+    {
+        name: 'description',
+        type: 'input',
+        message: 'Please add a description of your project:'
+    },
+    {
+        name: 'installation',
+        type: 'input',
+        message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.'
+    },
+    {
+        name: 'license',
+        type: 'checkbox',
+        message: 'Would you like to use any of the badges below for your README file?',
+        choices: ['L1', 'L2', 'L3', 'L4', 'L5']
+    },
+    {
+        name: 'usage',
+        type: 'input',
+        message: 'Provide instructions and examples for use. Include screenshots as needed.'
+    },
+    {
+        name: 'contribution',
+        type: 'input',
+        message: 'If you created an application or package and would like other developers to contribute it, you will want to add guidelines for how to do so.'
+    },
+    {
+        name: 'tests',
+        type: 'input',
+        message: 'Go the extra mile and write tests for your application. Then provide examples on how to run them.'
     }
 ];
 
