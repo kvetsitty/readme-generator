6ed7139d1b14b47c64e86aefc9359987bc325318
n1zhon1 <kristineetsitty@gmail.com>
2021-10-03 22:20:48 -0700

    added to badge render

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -3,7 +3,7 @@
 function renderLicenseBadge(license) {
   if (license !== "no license") {
     return `
-    ![badge](https://img.shields.io/badge/license-${license}-blue)
+    [![badge](https://img.shields.io/badge/license-${license}-blue)
     `;
   } else {
     return " ";
